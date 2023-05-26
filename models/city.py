99a9baa6add6d23ad57f741e658f48a62c132467
GiddyLesGid <gideonlangat@gmail.com>
2023-05-26 02:34:44 -0700

    airbnb

--- models/city.py
+++ models/city.py
@@ -4,7 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 
